9c1820dbf67284b9b6390753f55ae9c33c009f48
Anshul Goyal <anshulgoyalji@gmail.com>
2025-03-21 11:08:22 +0530

    Project Setup

---
+++ src/main/java/com/jenkins/JenkinsApp.java
@@ -0,0 +1,9 @@
+package com.jenkins;
+
+public class JenkinsApp {
+
+	public static void main(String[] args) {
+		System.out.println("Application Started...");
+	}
+
+}
---
+++ src/main/java/com/jenkins/entity/User.java
@@ -0,0 +1,32 @@
+package com.jenkins.entity;
+
+public class User {
+
+	private String uname;
+	private Integer age;
+	private String email;
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
